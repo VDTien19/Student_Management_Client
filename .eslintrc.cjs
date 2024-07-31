444373d0adc7341442d004a16bc38ae64fcbf69e
tiennehihi <vuductientienti@gmail.com>
2024-07-31 19:23:24 +0700

    fix gạch chân đỏ khi khai báo biến hoặc import module mà không dùng tới (trong file .eslintrc.cjs)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,25 @@
+// module.exports = {
+//   root: true,
+//   env: { browser: true, es2020: true },
+//   extends: [
+//     'eslint:recommended',
+//     'plugin:react/recommended',
+//     'plugin:react/jsx-runtime',
+//     'plugin:react-hooks/recommended',
+//   ],
+//   ignorePatterns: ['dist', '.eslintrc.cjs'],
+//   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+//   settings: { react: { version: '18.2' } },
+//   plugins: ['react-refresh'],
+//   rules: {
+//     'react/jsx-no-target-blank': 'off',
+//     'react-refresh/only-export-components': [
+//       'warn',
+//       { allowConstantExport: true },
+//     ],
+//   },
+// }
+
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
@@ -17,5 +39,14 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    // Bỏ qua cảnh báo cho biến chưa sử dụng
+    'no-unused-vars': [
+      'warn',
+      {
+        vars: 'all', // Kiểm tra tất cả biến
+        args: 'after-used', // Kiểm tra các tham số hàm sau khi sử dụng
+        ignoreRestSiblings: true, // Bỏ qua các phần còn lại khi destructuring
+      },
+    ],
   },
-}
+};
