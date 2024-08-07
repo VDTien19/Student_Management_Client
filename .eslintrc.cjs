40204cd280ec7342ad1540820c25fbbddf9aaed2
tiennehihi <vuductientienti@gmail.com>
2024-08-07 23:00:23 +0700

    bug: fix chuẩn cấu trúc thư mục và bỏ gạch chân đỏ với những thành phần chưa dùng tới

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,21 +1,79 @@
+// module.exports = {
+//   root: true,
+//   env: { browser: true, es2020: true },
+//   extends: [
+//     'eslint:recommended',
+//     'plugin:react/recommended',
+//     'plugin:react/jsx-runtime',
+//     'plugin:react-hooks/recommended',
+//   ],
+//   ignorePatterns: ['dist', '.eslintrc.cjs'],
+//   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+//   settings: { react: { version: '18.2' } },
+//   plugins: ['react-refresh'],
+//   rules: {
+//     'react/jsx-no-target-blank': 'off',
+//     'react-refresh/only-export-components': [
+//       'warn',
+//       { allowConstantExport: true },
+//     ],
+//   },
+// }
+
+// module.exports = {
+//   root: true,
+//   env: { browser: true, es2020: true },
+//   extends: [
+//     'eslint:recommended',
+//     'plugin:react/recommended',
+//     'plugin:react/jsx-runtime',
+//     'plugin:react-hooks/recommended',
+//   ],
+//   ignorePatterns: ['dist', '.eslintrc.cjs'],
+//   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+//   settings: { react: { version: '18.2' } },
+//   plugins: ['react-refresh'],
+//   rules: {
+//     'react/jsx-no-target-blank': 'off',
+//     'react-refresh/only-export-components': [
+//       'warn',
+//       { allowConstantExport: true },
+//     ],
+//     // Bỏ qua cảnh báo cho biến chưa sử dụng
+//     'no-unused-vars': [
+//       'warn',
+//       {
+//         vars: 'all', // Kiểm tra tất cả biến
+//         args: 'after-used', // Kiểm tra các tham số hàm sau khi sử dụng
+//         ignoreRestSiblings: true, // Bỏ qua các phần còn lại khi destructuring
+//       },
+//     ],
+//   },
+// };
+
+
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2021: true,
+  },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react/jsx-no-target-blank': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-}
+  plugins: [
+    'react',
+  ],
+  rules: {
+    // Bỏ qua cảnh báo cho biến chưa sử dụng
+    'no-unused-vars': ['warn', { vars: 'all', args: 'after-used', ignoreRestSiblings: true }],
+  },
+};
+
--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
 
+// eslint-disable-next-line react/prop-types
 const Chart = ({ title }) => {
   return (
     <Card className="mb-4">
