16e4094365e0f154aeeb111dbc47319142f028fe
tiennehihi <vuductientienti@gmail.com>
2024-08-31 02:01:49 +0700

    create cpn Modal to support display form

---
+++ src/components/modal/Modal.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './Modal.css'
+
+const Modal = ({ isOpen, onClose, children }) => {
+  if (!isOpen) return null;
+
+  return ReactDOM.createPortal(
+    <div className="modal-overlay">
+      <div className="modal-content">
+        <button
+          onClick={onClose}
+          className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
+        >
+          &times;
+        </button>
+        {children}
+      </div>
+    </div>,
+    document.body
+  );
+};
+
+export default Modal;
